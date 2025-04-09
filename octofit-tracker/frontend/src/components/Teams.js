a6288dea67299768ae0884d207abcc8277fa0742
reza azizi <azizi.ra@gmail.com>
2025-04-09 07:17:19 +0000

    atep 5 done

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ominous-telegram-7vvg9wp6993xg64-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="display-4">Teams</h1>
+      <ul className="list-group">
+        {teams.map(team => (
+          <li key={team._id} className="list-group-item">{team.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Teams;
